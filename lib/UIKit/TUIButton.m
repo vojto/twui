4e1a97f4b93c8d8f706d1a0f74195022565b9764
Danny Greg <danny@github.com>
2012-08-07 20:48:44 +0100

    Make the imageView of a TUIButton useful by default.

--- lib/UIKit/TUIButton.m
+++ lib/UIKit/TUIButton.m
@@ -80,6 +80,9 @@
 - (void)setTitleEdgeInsets:(TUIEdgeInsets)i
 {
 	_titleEdgeInsets = i;
+	if (_imageView != nil) {
+		_imageView.frame = TUIEdgeInsetsInsetRect(self.bounds, self.imageEdgeInsets);
+	}
 }
 
 - (TUIEdgeInsets)titleEdgeInsets
@@ -107,9 +110,9 @@
 - (TUIImageView *)imageView
 {
 	if(!_imageView) {
-		_imageView = [[TUIImageView alloc] initWithFrame:CGRectZero];
+		_imageView = [[TUIImageView alloc] initWithFrame:TUIEdgeInsetsInsetRect(self.bounds, self.imageEdgeInsets)];
 		_imageView.backgroundColor = [TUIColor clearColor];
-		_imageView.hidden = YES;
+		[self addSubview:_imageView];
 	}
 	return _imageView;
 }

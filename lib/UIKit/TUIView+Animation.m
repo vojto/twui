96a7042a7a7c4a91afe6cb477421f1092e83e24f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-30 16:17:04 -0700

    Style fixes

--- lib/UIKit/TUIView+Animation.m
+++ lib/UIKit/TUIView+Animation.m
@@ -33,48 +33,49 @@
 
 @implementation TUIViewAnimation
 
-- (id)init
-{
-	if((self = [super init]))
-	{
-		basicAnimation = [CABasicAnimation animation];
-	}
+- (id)init {
+	self = [super init];
+	if (self == nil) return nil;
+
+	_basicAnimation = [CABasicAnimation animation];
 	return self;
 }
 
-- (void)dealloc
-{
-	if(animationCompletionBlock != nil) {
-		animationCompletionBlock(NO);
+- (void)dealloc {
+	if (self.animationCompletionBlock != nil) {
+		self.animationCompletionBlock(NO);
 		NSLog(@"Error: animation completion block didn't complete!");
 	}
 }
 
-- (void)runActionForKey:(NSString *)event object:(id)anObject arguments:(NSDictionary *)dict
-{
-	CAAnimation *animation = [basicAnimation copyWithZone:nil];
+- (void)runActionForKey:(NSString *)event object:(id)anObject arguments:(NSDictionary *)dict {
+	CAAnimation *animation = [self.basicAnimation copyWithZone:nil];
 	animation.delegate = self;
 	[animation runActionForKey:event object:anObject arguments:dict];
 }
 
-- (void)animationDidStart:(CAAnimation *)anim
-{
-	if(delegate && animationWillStartSelector) {
-		void (*animationWillStartIMP)(id,SEL,NSString*,void*) = (void(*)(id,SEL,NSString*,void*))[(NSObject *)delegate methodForSelector:animationWillStartSelector];
-		animationWillStartIMP(delegate, animationWillStartSelector, animationID, context);
-		animationWillStartSelector = NULL; // only fire this once
-	}
+- (void)animationDidStart:(CAAnimation *)anim {
+	if (self.delegate == nil || self.animationWillStartSelector == NULL) return;
+
+	void (*animationWillStartIMP)(id, SEL, NSString *, void *) = (__typeof__(animationWillStartIMP))[(id)self.delegate methodForSelector:self.animationWillStartSelector];
+	animationWillStartIMP(self.delegate, self.animationWillStartSelector, self.animationID, self.context);
+
+	// only fire this once
+	self.animationWillStartSelector = NULL;
 }
 
-- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
-{
-	if(delegate && animationDidStopSelector) {
-		void (*animationDidStopIMP)(id,SEL,NSString*,NSNumber*,void*) = (void(*)(id,SEL,NSString*,NSNumber*,void*))[(NSObject *)delegate methodForSelector:animationDidStopSelector];
-		animationDidStopIMP(delegate, animationDidStopSelector, animationID, [NSNumber numberWithBool:flag], context);
-		animationDidStopSelector = NULL; // only fire this once
-	} else if(animationCompletionBlock) {
-		animationCompletionBlock(flag);
-		self.animationCompletionBlock = nil; // only fire this once
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+	if (self.delegate != nil && self.animationDidStopSelector != NULL) {
+		void (*animationDidStopIMP)(id, SEL, NSString *, NSNumber *, void *) = (__typeof__(animationDidStopIMP))[(id)self.delegate methodForSelector:self.animationDidStopSelector];
+		animationDidStopIMP(self.delegate, self.animationDidStopSelector, self.animationID, @(flag), self.context);
+
+		// only fire this once
+		self.animationDidStopSelector = NULL;
+	} else if (self.animationCompletionBlock) {
+		self.animationCompletionBlock(flag);
+
+		// only fire this once
+		self.animationCompletionBlock = nil;
 	}
 }
 
@@ -84,34 +85,30 @@
 
 static NSMutableArray *AnimationStack = nil;
 
-+ (NSMutableArray *)_animationStack
-{
-	if(!AnimationStack)
-		AnimationStack = [[NSMutableArray alloc] init];
++ (NSMutableArray *)_animationStack {
+	if(!AnimationStack) AnimationStack = [[NSMutableArray alloc] init];
 	return AnimationStack;
 }
 
-+ (TUIViewAnimation *)_currentAnimation
-{
++ (TUIViewAnimation *)_currentAnimation {
 	return [AnimationStack lastObject];
 }
 
-+ (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations
-{
++ (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations {
 	[self animateWithDuration:duration animations:animations completion:NULL];
 }
 
-+ (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion
-{
++ (void)animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations completion:(void (^)(BOOL finished))completion {
 	[self beginAnimations:nil context:NULL];
-	[self setAnimationDuration:duration];
-	[[self _currentAnimation] setAnimationCompletionBlock:completion];
+	self.animationDuration = duration;
+	[self _currentAnimation].animationCompletionBlock = completion;
+
 	animations();
+
 	[self commitAnimations];
 }
 
-+ (void)beginAnimations:(NSString *)animationID context:(void *)context
-{
++ (void)beginAnimations:(NSString *)animationID context:(void *)context {
 	[NSAnimationContext beginGrouping];
 
 	TUIViewAnimation *animation = [[TUIViewAnimation alloc] init];
@@ -120,60 +117,54 @@ static NSMutableArray *AnimationStack = nil;
 	[[self _animationStack] addObject:animation];
 	
 	// setup defaults
-	[self setAnimationDuration:0.25];
-	[self setAnimationCurve:TUIViewAnimationCurveEaseInOut];
+	self.animationDuration = 0.25;
+	self.animationCurve = TUIViewAnimationCurveEaseInOut;
 }
 
-+ (void)commitAnimations
-{
++ (void)commitAnimations {
 	[[self _animationStack] removeLastObject];
 	[NSAnimationContext endGrouping];
 }
 
-+ (void)setAnimationDelegate:(id)delegate
-{
++ (void)setAnimationDelegate:(id)delegate {
 	[self _currentAnimation].delegate = delegate;
 }
 
-+ (void)setAnimationWillStartSelector:(SEL)selector
-{
++ (void)setAnimationWillStartSelector:(SEL)selector {
 	[self _currentAnimation].animationWillStartSelector = selector;
 }
 
-+ (void)setAnimationDidStopSelector:(SEL)selector
-{
++ (void)setAnimationDidStopSelector:(SEL)selector {
 	[self _currentAnimation].animationDidStopSelector = selector;
 }
 
-static CGFloat SlomoTime()
-{
-	if((NSUInteger)([NSEvent modifierFlags]&NSDeviceIndependentModifierFlagsMask) == (NSUInteger)(NSShiftKeyMask))
+static CGFloat SlomoTime() {
+	if (([NSEvent modifierFlags] & NSDeviceIndependentModifierFlagsMask) == NSShiftKeyMask) {
 		return 5.0;
-	return 1.0;
+	} else {
+		return 1.0;
+	}
 }
 
-+ (void)setAnimationDuration:(NSTimeInterval)duration
-{
++ (void)setAnimationDuration:(NSTimeInterval)duration {
 	duration *= SlomoTime();
 	[self _currentAnimation].basicAnimation.duration = duration;
 	[NSAnimationContext currentContext].duration = duration;
 }
 
-+ (void)setAnimationDelay:(NSTimeInterval)delay
-{
++ (void)setAnimationDelay:(NSTimeInterval)delay {
 	[self _currentAnimation].basicAnimation.beginTime = CACurrentMediaTime() + delay * SlomoTime();
 	[self _currentAnimation].basicAnimation.fillMode = kCAFillModeBoth;
 }
 
-+ (void)setAnimationStartDate:(NSDate *)startDate
-{
++ (void)setAnimationStartDate:(NSDate *)startDate {
 	NSAssert(NO, @"%s is not yet implemented", __func__);
 }
 
-+ (void)setAnimationCurve:(TUIViewAnimationCurve)curve
-{
++ (void)setAnimationCurve:(TUIViewAnimationCurve)curve {
 	NSString *functionName = kCAMediaTimingFunctionEaseInEaseOut;
-	switch(curve) {
+
+	switch (curve) {
 		case TUIViewAnimationCurveLinear:
 			functionName = kCAMediaTimingFunctionLinear;
 			break;
@@ -186,91 +177,80 @@ static CGFloat SlomoTime()
 		case TUIViewAnimationCurveEaseInOut:
 			functionName = kCAMediaTimingFunctionEaseInEaseOut;
 			break;
+		default:
+			NSAssert(NO, @"Unrecognized animation curve: %i", (int)curve);
 	}
+
 	[self _currentAnimation].basicAnimation.timingFunction = [CAMediaTimingFunction functionWithName:functionName];
 }
 
-+ (void)setAnimationRepeatCount:(float)repeatCount
-{
++ (void)setAnimationRepeatCount:(float)repeatCount {
 	[self _currentAnimation].basicAnimation.repeatCount = repeatCount;
 }
 
-+ (void)setAnimationRepeatAutoreverses:(BOOL)repeatAutoreverses
-{
++ (void)setAnimationRepeatAutoreverses:(BOOL)repeatAutoreverses {
 	[self _currentAnimation].basicAnimation.autoreverses = repeatAutoreverses;
 }
 
-+ (void)setAnimationIsAdditive:(BOOL)additive
-{
++ (void)setAnimationIsAdditive:(BOOL)additive {
 	[self _currentAnimation].basicAnimation.additive = additive;
 }
 
-+ (void)setAnimationBeginsFromCurrentState:(BOOL)fromCurrentState
-{
++ (void)setAnimationBeginsFromCurrentState:(BOOL)fromCurrentState {
 	NSAssert(NO, @"%s is not yet implemented", __func__);
 }
 
-+ (void)setAnimationTransition:(TUIViewAnimationTransition)transition forView:(TUIView *)view cache:(BOOL)cache
-{
++ (void)setAnimationTransition:(TUIViewAnimationTransition)transition forView:(TUIView *)view cache:(BOOL)cache {
 	NSAssert(NO, @"%s is not yet implemented", __func__);
 }
 
 static BOOL disableAnimations = NO;
 
-+ (void)setAnimationsEnabled:(BOOL)enabled block:(void(^)(void))block
-{
++ (void)setAnimationsEnabled:(BOOL)enabled block:(void(^)(void))block {
 	BOOL save = disableAnimations;
 	disableAnimations = !enabled;
 	block();
 	disableAnimations = save;
 }
 
-+ (void)setAnimationsEnabled:(BOOL)enabled
-{
++ (void)setAnimationsEnabled:(BOOL)enabled {
 	disableAnimations = !enabled;
 }
 
-+ (BOOL)areAnimationsEnabled
-{
++ (BOOL)areAnimationsEnabled {
 	return !disableAnimations;
 }
 
 static BOOL animateContents = NO;
 
-+ (void)setAnimateContents:(BOOL)enabled
-{
++ (void)setAnimateContents:(BOOL)enabled {
 	animateContents = enabled;
 }
 
-+ (BOOL)willAnimateContents
-{
++ (BOOL)willAnimateContents {
 	return animateContents;
 }
 
-- (void)removeAllAnimations
-{
+- (void)removeAllAnimations {
 	[self.layer removeAllAnimations];
 	[self.subviews makeObjectsPerformSelector:@selector(removeAllAnimations)];
 }
 
-- (id<CAAction>)actionForLayer:(CALayer *)layer forKey:(NSString *)event
-{
+- (id<CAAction>)actionForLayer:(CALayer *)layer forKey:(NSString *)event {
 	id defaultAction = [NSNull null];
+	if (disableAnimations) return defaultAction;
 
-	if(disableAnimations)
-		return defaultAction;
-
-	if((animateContents == NO) && [event isEqualToString:@"contents"])
-		return defaultAction; // default - don't animate contents
+	// default - don't animate contents
+	if (!animateContents && [event isEqualToString:@"contents"]) return defaultAction;
 
 	id animation = [TUIView _currentAnimation];
-	if (!animation)
-		return defaultAction;
+	if (animation == nil) return defaultAction;
 
-	if ([TUICAAction interceptsActionForKey:event])
+	if ([TUICAAction interceptsActionForKey:event]) {
 		return [TUICAAction actionWithAction:animation];
-	else
+	} else {
 		return animation;
+	}
 }
 
 @end

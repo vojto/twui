aef468263333d0577a91f3bd56fc5ebfc14567f6
galaxas0 <aditya.nrusimha@gmail.com>
2012-10-02 13:21:58 -0700

    Fixed getter syntax.

--- lib/UIKit/TUIScrollKnob.m
+++ lib/UIKit/TUIScrollKnob.m
@@ -72,7 +72,7 @@ static NSTimeInterval const TUIScrollIndicatorStateRefreshSpeed = 0.01f;
 - (void)refreshKnobTimer {
 	if([NSScroller preferredScrollerStyle] == NSScrollerStyleOverlay) {
 		TUIScrollViewIndicatorVisibility visibility;
-		if(self.isVertical)
+		if(self.vertical)
 			visibility = scrollView.verticalScrollIndicatorVisibility;
 		else
 			visibility = scrollView.horizontalScrollIndicatorVisibility;

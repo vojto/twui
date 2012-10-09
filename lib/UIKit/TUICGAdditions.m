e7a3d71b5fe20ac6dc0adbd356958f308b36eddf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-10-08 17:18:19 -0700

    Use screen scale for TUIGraphicsDrawAsImage()

--- lib/UIKit/TUICGAdditions.m
+++ lib/UIKit/TUICGAdditions.m
@@ -246,7 +246,7 @@ void TUIGraphicsEndImageContext(void)
 
 NSImage *TUIGraphicsDrawAsImage(CGSize size, void(^draw)(void))
 {
-	TUIGraphicsBeginImageContext(size);
+	TUIGraphicsBeginImageContextWithOptions(size, NO, 0);
 	draw();
 	NSImage *image = TUIGraphicsGetImageFromCurrentImageContext();
 	TUIGraphicsEndImageContext();

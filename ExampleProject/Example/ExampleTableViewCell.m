a54d25b54ac51cc9024602d766e4b65721f0d635
Aditya Nrusimha <aditya.nrusimha@gmail.com>
2012-10-11 21:12:38 -0700

    Cleaned up example to show minimal customization.

--- ExampleProject/Example/ExampleTableViewCell.m
+++ ExampleProject/Example/ExampleTableViewCell.m
@@ -20,10 +20,28 @@
 
 - (id)initWithStyle:(TUITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
 	if((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+		
+		// Set our indentation level to let the text renderer pad its frame.
+		self.indentationLevel = 1;
+		
+		// Set up the alternating row "look". Very slick, clean look. Just by
+		// setting up a few colors (and optionally a few styles, and if we
+		// need more, overridable methods) we can achieve whatever look we want.
+		self.backgroundColor = [NSColor colorWithCalibratedWhite:0.97 alpha:1.0f];
+		self.highlightColor = [NSColor colorWithCalibratedWhite:0.87 alpha:1.0f];
+		self.alternateBackgroundColor = [NSColor colorWithCalibratedWhite:0.92 alpha:1.0f];
+		self.alternateHighlightColor = [NSColor colorWithCalibratedWhite:0.82 alpha:1.0f];
+		
+		// Instead of using a TUILabel or TUITextField, let's take a little
+		// course in the art of using a TUITextRenderer. It acts as a simple
+		// text -> layer rendering mechanism class with tie-ins to TUIView,
+		// but can achieve far more if used right (like TUITextView).
 		_textRenderer = [[TUITextRenderer alloc] init];
 		_textRenderer.verticalAlignment = TUITextVerticalAlignmentMiddle;
 		_textRenderer.shouldRefuseFirstResponder = YES;
 		_textRenderer.shadowBlur = 1.0f;
+		_textRenderer.shadowColor = [NSColor whiteColor];
+		_textRenderer.shadowOffset = CGSizeMake(0, 1);
 		
 		// Add the text renderer to the view so events get routed to it
 		// properly. Text selection, dictionary popup, etc should just work.
@@ -34,13 +52,9 @@
 		// be set before it can be drawn, we do that in drawRect: below.
 		self.textRenderers = [NSArray arrayWithObjects:_textRenderer, nil];
 		
-		//self.selectionStyle = TUITableViewCellColorStyleGray;
-		//self.coalescenceAngle = TUITableViewCellAngleDown;
-		//self.backgroundColor = [NSColor colorWithCalibratedWhite:0.97 alpha:1.0f];
-		//self.alternateBackgroundColor = [NSColor colorWithCalibratedWhite:0.92 alpha:1.0f];
-		//self.highlightColor = [NSColor colorWithCalibratedWhite:0.87 alpha:1.0f];
-		//self.animatesStyleChanges = YES;
-		
+		// Add in a standard Cocoa text field. We have to enclose this within
+		// a TUIViewNSViewContainer, and we MUST adjust only the frame of that
+		// container, and not the text field's itself.
 		NSTextField *textField = [[NSTextField alloc] initWithFrame:NSMakeRect(20, 180, 91, 22)];
 		[textField.cell setUsesSingleLineMode:YES];
 		[textField.cell setScrollable:YES];
@@ -53,6 +67,7 @@
 	return self;
 }
 
+// Create a simple attributed string setter/getter to the text renderer.
 - (NSAttributedString *)attributedString {
 	return _textRenderer.attributedString;
 }
@@ -62,40 +77,30 @@
 	[self setNeedsDisplay];
 }
 
+// When we subclass any view and override the layout or drawing methods,
+// it's a must to insert a super call before doing your own thing.
 - (void)layoutSubviews {
 	[super layoutSubviews];
 	
+	// Set the text field container's frame (NOT THE TEXT FIELD).
 	CGSize textFieldSize = self.textFieldContainer.bounds.size;
 	CGFloat textFieldLeft = CGRectGetWidth(self.bounds) - textFieldSize.width - 16;
 	self.textFieldContainer.frame = CGRectMake(textFieldLeft, 14, textFieldSize.width, textFieldSize.height);
+	
+	// Set the text renderer's frame. Take indentation into account.
+	CGFloat totalIndentation = self.indentationWidth * self.indentationLevel;
+	CGRect textRect = self.bounds;
+	textRect.origin.x += totalIndentation;
+	textRect.size.width -= (self.textFieldContainer.frame.size.width + 16) +
+						   (totalIndentation + self.indentationWidth);
+	_textRenderer.frame = textRect;
 }
 
+// Note that we only override the drawRect: to add in a call to
+// let the text renderer draw as well. For TUITableView, customization
+// is preferrably left to the overridable drawing methods - not this.
 - (void)drawRect:(CGRect)rect {
 	[super drawRect:rect];
-	
-	// Change the text color when state changes, so it looks alright.
-	if(self.highlighted || self.selected) {
-		TUIAttributedString *string = (TUIAttributedString *)_textRenderer.attributedString;
-		[string setColor:[NSColor whiteColor]];
-		_textRenderer.attributedString = string;
-		
-		_textRenderer.shadowColor = [NSColor blackColor];
-		_textRenderer.shadowOffset = CGSizeMake(0, -1);
-	} else {
-		TUIAttributedString *string = (TUIAttributedString *)_textRenderer.attributedString;
-		[string setColor:[NSColor blackColor]];
-		_textRenderer.attributedString = string;
-		
-		_textRenderer.shadowColor = [NSColor whiteColor];
-		_textRenderer.shadowOffset = CGSizeMake(0, 1);
-	}
-	
-	// Set the frame so it knows where to draw itself.
-	CGRect textRect = self.bounds;
-	textRect.origin.x += self.indentationWidth;
-	textRect.size.width -= (self.textFieldContainer.frame.size.width + 16) + (self.indentationWidth * 2);
-	
-	_textRenderer.frame = textRect;
 	[_textRenderer draw];
 }
 

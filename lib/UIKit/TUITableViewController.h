c4c7a14e66bb4a1ae4f66d3c773dbf8b0cfe2d1e
Aditya Nrusimha <aditya.nrusimha@gmail.com>
2012-10-17 22:12:01 -0700

    Added documentation.

--- lib/UIKit/TUITableViewController.h
+++ lib/UIKit/TUITableViewController.h
@@ -17,11 +17,40 @@
 #import "TUIViewController.h"
 #import "TUITableView.h"
 
+// The TUITableViewController is a controller that manages a table view.
+// When the table view is about to appear the first time it’s loaded,
+// the table-view controller reloads the table view’s data. It also
+// clears its selection (with or without animation, depending on the
+// request) every time the table view is displayed. You can disable
+// this behavior by changing the value in the clearsSelectionOnViewWillAppear
+// property. When the table view has appeared, the controller flashes
+// the table view’s scroll indicators. You create a custom subclass of
+// TUITableViewController for each table view that you want to manage.
+// When you initialize the controller in initWithStyle:, you must specify
+// the style of the table view (plain or grouped) that the controller is
+// to manage. Because the initially created table view is without table
+// dimensions (that is, number of sections and number of rows per section)
+// or content, the table view’s data source and delegate — that is, the
+// TUITableViewController itself — must provide the table dimensions,
+// the cell content, and any desired configurations as usual. You may
+// override loadView or any other superclass method, but if you do be
+// sure to invoke the superclass implementation of the method first.
 @interface TUITableViewController : TUIViewController <TUITableViewDelegate, TUITableViewDataSource>
 
+// Returns the table view managed by the controller.
 @property (nonatomic, strong) TUITableView *tableView;
+
+// A Boolean value indicating if the controller clears the selection when
+// the table appears. When YES, the table view controller clears the
+// table’s current selection when it receives a viewWillAppear: message.
+// Setting this property to NO preserves the selection. Defaults to YES.
 @property (nonatomic, assign) BOOL clearsSelectionOnViewWillAppear;
 
+// Initializes a table view controller to manage a table view of a given style.
+// The style specifies the style of table view that the controller is to
+// manage (plain or grouped). Returns an initialized TUITableViewController.
+// If you use the standard init method to initialize a controller, a
+// table view in the plain style is created.
 - (id)initWithStyle:(TUITableViewStyle)style;
 
 @end
